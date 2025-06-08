2789de7f749b1f64ef0e3d5b43f0a4793a2edbfe
Daniel Howorth <daniel.howorth@sky.com>
2025-06-08 13:52:37 +0100

    ModalProvider provides modal component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import styles from "./Layout.module.css";
 import Header from "../Header";
-import Modal from "../Modal";
 import { ModalContext } from "../../providers/ModalProvider";
 
 function Layout() {
-  const { isModalOpen, toggleIsModalOpen } = React.use(ModalContext);
+  const { modal, isModalOpen } = React.use(ModalContext);
 
   return (
     <div className={styles.layout}>
-      {isModalOpen && (
-        <Modal
-          isOpen={isModalOpen}
-          handleDismiss={() => toggleIsModalOpen(false)}
-        >
-          <div>Hello, World!</div>
-        </Modal>
-      )}
+      {isModalOpen && modal}
       <Header />
       <section></section>
       <section></section>
--- src/providers/ModalProvider/ModalProvider.jsx
+++ src/providers/ModalProvider/ModalProvider.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "../../components/Modal";
 import useToggle from "../../hooks/useToggle";
 
 export const ModalContext = React.createContext();
@@ -6,7 +7,13 @@ export const ModalContext = React.createContext();
 function ModalProvider({ children }) {
   const [isModalOpen, toggleIsModalOpen] = useToggle();
 
-  const value = { isModalOpen, toggleIsModalOpen };
+  const modal = (
+    <Modal isOpen={isModalOpen} handleDismiss={() => toggleIsModalOpen(false)}>
+      <div>Hello, World!</div>
+    </Modal>
+  );
+
+  const value = { modal, isModalOpen, toggleIsModalOpen };
 
   return <ModalContext value={value}>{children}</ModalContext>;
 }
