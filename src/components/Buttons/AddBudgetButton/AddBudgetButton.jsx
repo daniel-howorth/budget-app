5c2915877c46e16e0d20ba2e8255a707bddb36bc
Daniel Howorth <daniel.howorth@sky.com>
2025-06-12 13:08:30 +0100

    Removed redundant alt from AddBudgetButton

--- src/components/Buttons/AddBudgetButton/AddBudgetButton.jsx
+++ src/components/Buttons/AddBudgetButton/AddBudgetButton.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import PrimaryButton from "../PrimaryButton";
 import { ModalContext } from "../../../providers/ModalProvider";
 
-function AddBudgetButton({ alt, className = "", ...delegated }) {
+function AddBudgetButton({ className = "", ...delegated }) {
   const { toggleIsModalOpen, setModalAction } = React.use(ModalContext);
 
   return (
     <PrimaryButton
-      alt={alt}
       className={className}
       onClick={() => {
         setModalAction("add-budget");
