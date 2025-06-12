906dbf0b3526cf0b5044309346b5efe3374f131f
Daniel Howorth <daniel.howorth@sky.com>
2025-06-12 11:51:10 +0100

    Implemented NumberInput

---
+++ src/components/FormElements/NumberInput/NumberInput.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import LabeledInput from "../LabeledInput";
+import styles from "./NumberInput.module.css";
+import clsx from "clsx";
+
+function NumberInput({
+  id,
+  label,
+  required = false,
+  min = 0,
+  step = 0.01,
+  inputMode = "decimal",
+
+  className = "",
+  ...delegated
+}) {
+  const appliedClassName = clsx(
+    styles.input,
+    "form-element-padding",
+    "tap-target",
+    className
+  );
+
+  return (
+    <LabeledInput id={id} label={label} required={required}>
+      <input
+        type="number"
+        min={min}
+        step={step}
+        inputMode={inputMode}
+        className={appliedClassName}
+        {...delegated}
+        pattern="[0-9]*"
+      />
+    </LabeledInput>
+  );
+}
+
+export default NumberInput;
--- src/components/Forms/AddBudgetForm/AddBudgetForm.jsx
+++ src/components/Forms/AddBudgetForm/AddBudgetForm.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Form from "../Form";
 import TextInput from "../../FormElements/TextInput";
+import NumberInput from "../../FormElements/NumberInput";
 
 function AddBudgetForm() {
   return (
     <Form>
       <h1>Add Budget</h1>
-      <TextInput id={"name"} label="Name" placeholder="Groceries" required />
+      <TextInput
+        id={"name"}
+        label="Name"
+        placeholder="Groceries"
+        required
+        className="margin-bottom--md"
+      />
+      <NumberInput id="limit" label="Limit" placeholder="100" required />
     </Form>
   );
 }
