425c5bc44220830ce10675cf7a46928c25536c5c
Daniel Howorth <daniel.howorth@sky.com>
2025-06-24 17:48:49 +0100

    ViewExpensesButton loads budget expenses and passes them to ExpensesList

--- src/components/Buttons/ViewExpensesButton/ViewExpensesButton.jsx
+++ src/components/Buttons/ViewExpensesButton/ViewExpensesButton.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import PrimaryButton from "../PrimaryButton";
 import { ModalContext } from "../../../providers/ModalProvider";
+import { BudgetsContext } from "../../../providers/BudgetsProvider";
 import ExpensesList from "../../ExpensesList";
 
 function ViewExpensesButton({ actionTarget, className = "" }) {
-  const { openModal, setActionTarget } = React.use(ModalContext);
+  const { openModal } = React.use(ModalContext);
+  const { getBudget } = React.use(BudgetsContext);
+
+  const budget = getBudget(actionTarget);
+  const expenses = budget.expenses;
 
   return (
     <PrimaryButton
       className={className}
       onClick={() => {
-        setActionTarget(actionTarget);
-        openModal(<ExpensesList />);
+        openModal(<ExpensesList expenses={expenses} />);
       }}
     >
       View Expenses
--- src/components/ExpensesList/ExpensesList.jsx
+++ src/components/ExpensesList/ExpensesList.jsx
@@ -3,14 +3,10 @@ import Expense from "./Expense";
 import styles from "./ExpensesList.module.css";
 
 function ExpensesList({ expenses }) {
-  expenses = [
-    { item: "coffee", value: 10 },
-    { item: "ice-cream", value: 10 },
-  ];
   return (
     <dl className={styles["expenses-list"]}>
-      {expenses.map(({ item, value }) => (
-        <Expense item={item} value={value} />
+      {expenses.map(({ id, item, value }) => (
+        <Expense key={id} item={item} value={value} />
       ))}
     </dl>
   );
